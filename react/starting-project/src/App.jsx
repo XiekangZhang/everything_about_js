c60517839d9227243dfdd7901b5f161409e5281d
Zhang <xiekang.zhang@adesso.de>
2024-02-16 15:57:23 +0100

    starting udemy nextjs tutorial

---
+++ react/starting-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post.jsx";
+
+function App() {
+    return (
+        <main>
+            <Post author="Zhang" body="React.js is awesome!"/>
+            <Post author="Li" body="Nice tutorial!"/>
+        </main>
+    );
+}
+
+export default App;
