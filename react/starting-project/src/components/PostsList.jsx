b2c3b0ee2a9e28951fc4f64ff835c8daa8e56e97
Xiekang Zhang <zhangxiekang@gmail.com>
2024-02-19 21:39:31 +0100

    starting backend

--- react/starting-project/src/components/PostsList.jsx
+++ react/starting-project/src/components/PostsList.jsx
@@ -1,31 +1,42 @@
 import Post from "./Post.jsx";
 import styles from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
-import {useState} from "react";
 import Modal from "./Modal.jsx";
+import { useState } from "react";
 
-function PostsList({isPosting, onStopPosting}) {
-    const [currentBody, setCurrentBody] = useState("");
-    const [currentAuthor, setCurrentAuthor] = useState("");
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
 
-    function changeBodyHandler(event) {
-        setCurrentBody(event.target.value);
-    }
+  function addPostHandler(postData) {
+    setPosts((prevPosts) => {
+      return [postData, ...prevPosts];
+    });
+  }
 
-    function changeAuthorHandler(event) {
-        setCurrentAuthor(event.target.value);
-    }
+  return (
+    <>
+      {isPosting && (
+        <Modal onHideModel={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
 
-    return (<>
-        {isPosting &&
-            <Modal onHideModel={onStopPosting}>
-                <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
-            </Modal>}
+      {posts.length > 0 && (
         <ul className={styles.posts}>
-            <Post author={currentAuthor} body={currentBody}/>
-            <Post author="Li" body="Nice tutorial!"/>
+          {posts.map((post) => (
+            <Post author={post.author} body={post.body} key={post.body} />
+          ))}
         </ul>
-    </>);
+      )}
+
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default PostsList;
