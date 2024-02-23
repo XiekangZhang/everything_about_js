0e99d363f1f1b825dbc2628baf1764a65af8d1c6
Zhang <xiekang.zhang@adesso.de>
2024-02-23 14:01:53 +0100

    finish homepage

--- next_world/starting-project/05-onwards-foodies-starting-project/components/main-header.js
+++ next_world/starting-project/05-onwards-foodies-starting-project/components/main-header.js
@@ -1,24 +1,29 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import styles from "@/components/main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header-background";
 
 export default function MainHeader() {
     return (
-        <header className={styles.header}>
-            <Link href="/" className={styles.logo}>
-                <img src={logoImg.src} alt="A plate with food on it"/>
-                NextLevel Food
-            </Link>
-            <nav className={styles.nav}>
-                <ul>
-                    <li>
-                        <Link href={"/meals"}>Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href={"/community"}>Foodies Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <MainHeaderBackground/>
+            <header className={styles.header}>
+                <Link href="/" className={styles.logo}>
+                    <Image src={logoImg} alt="A plate with food on it" priority/>
+                    NextLevel Food
+                </Link>
+                <nav className={styles.nav}>
+                    <ul>
+                        <li>
+                            <Link href={"/meals"}>Browse Meals</Link>
+                        </li>
+                        <li>
+                            <Link href={"/community"}>Foodies Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     );
 }
