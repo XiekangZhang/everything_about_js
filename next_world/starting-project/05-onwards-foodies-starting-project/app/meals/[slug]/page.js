249945441495f39a4438a409fefb6241a608f879
Xiekang Zhang <zhangxiekang@gmail.com>
2024-03-05 21:46:38 +0100

    adding dynamic routing pages

--- next_world/starting-project/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
+++ next_world/starting-project/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -1,5 +1,35 @@
-export default function MealDetailsPage() {
-    return (
-        <h1>Meal Details Page</h1>
-    );
+import { getMeal } from "@/app/lib/meals";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: `${meal.instructions.replace(/\n/g, '<br />')}` }}
+        ></p>
+      </main>
+    </>
+  );
 }
