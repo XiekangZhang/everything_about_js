43a846deb87205789bc5392cf8f1391235791472
Zhang <xiekang.zhang@adesso.de>
2024-03-22 15:11:39 +0100

    finish the nextjs essentials

--- next_world/starting-project/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
+++ next_world/starting-project/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -1,35 +1,47 @@
-import { getMeal } from "@/app/lib/meals";
+import {getMeal} from "@/app/lib/meals";
 import styles from "./page.module.css";
 import Image from "next/image";
-import { notFound } from "next/navigation";
+import {notFound} from "next/navigation";
 
-export default function MealDetailsPage({ params }) {
-  const meal = getMeal(params.slug);
+export async function generateMetadata({params}) {
+    const meal = getMeal(params.slug);
+    if (!meal) {
+        return notFound();
+    }
+    return {
+        title: meal.title,
+        description: meal.summary,
+    }
+}
 
-  if (!meal) {
-    notFound();
-  }
 
-  return (
-    <>
-      <header className={styles.header}>
-        <div className={styles.image}>
-          <Image src={meal.image} alt={meal.title} fill />
-        </div>
-        <div className={styles.headerText}>
-          <h1>{meal.title}</h1>
-          <p className={styles.creator}>
-            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-          </p>
-          <p className={styles.summary}>{meal.summary}</p>
-        </div>
-      </header>
-      <main>
-        <p
-          className={styles.instructions}
-          dangerouslySetInnerHTML={{ __html: `${meal.instructions.replace(/\n/g, '<br />')}` }}
-        ></p>
-      </main>
-    </>
-  );
+export default function MealDetailsPage({params}) {
+    const meal = getMeal(params.slug);
+
+    if (!meal) {
+        notFound();
+    }
+
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image src={meal.image} alt={meal.title} fill/>
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={styles.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={styles.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={styles.instructions}
+                    dangerouslySetInnerHTML={{__html: `${meal.instructions.replace(/\n/g, '<br />')}`}}
+                ></p>
+            </main>
+        </>
+    );
 }
