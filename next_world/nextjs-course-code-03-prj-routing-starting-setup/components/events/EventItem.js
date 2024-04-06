45c67d9f1bacbfb8ba122d89caecdb9efb80a312
xiekang <zhangxiekang@gmail.com>
2024-04-06 12:01:52 +0200

    finish udemy

--- next_world/nextjs-course-code-03-prj-routing-starting-setup/components/events/EventItem.js
+++ next_world/nextjs-course-code-03-prj-routing-starting-setup/components/events/EventItem.js
@@ -1,6 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
 import classes from "./EventItem.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AdressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem({ event }) {
   const humanReadableDate = new Date(event.date).toLocaleDateString("en-US", {
@@ -11,23 +13,27 @@ export default function EventItem({ event }) {
   const formattedAddress = event.location.replace(",", "\n");
   return (
     <li className={classes.item}>
-      <img
-        src={`/${event.image}`}
-        alt={event.title}
-      />
+      <img src={`/${event.image}`} alt={event.title} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{event.title}</h2>
           {/*<p>{event.description}</p>*/}
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AdressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div className={classes}>
-          <Link href={`/events/${event.id}`}>Explore Event</Link>
+        <div className={classes.actions}>
+          <Button link={`/events/${event.id}`}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
